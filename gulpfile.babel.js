597c02d12ba50d8f726fe2c052991a83599a1dea
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-12 12:50:41 -0700

    update gulp build task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,15 +5,19 @@ import rimraf from "rimraf";
 const $ = require("gulp-load-plugins")();
 
 gulp.task("server:clean", cb => {
-  rimraf("./build", () => cb());
+	rimraf("./build", () => cb());
 });
 
-gulp.task("server:build", () => {
-	return gulp.src("./src/server/**/*.js")
-    .pipe($.changed("./build"))
-    .pipe($.sourcemaps.init())
-    .pipe($.babel())
-    .pipe($.sourcemaps.write(".", {}))
-    .pipe($.sourcemaps.write(".",{sourceRoot: path.join(__dirname, "src", "server")}))
-    .pipe(gulp.dest("./build"));
-});
+gulp.task("server:build",
+    gulp.series(
+        "server:clean",
+        () => {
+            return gulp.src("./src/server/**/*.js")
+                .pipe($.changed("./build"))
+                .pipe($.sourcemaps.init())
+                .pipe($.babel())
+                .pipe($.sourcemaps.write(".", {}))
+                .pipe($.sourcemaps.write(".",{sourceRoot: path.join(__dirname, "src", "server")}))
+                .pipe(gulp.dest("./build"));
+        }
+ ));
