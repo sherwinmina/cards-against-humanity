e54f768f12f2a9559697ae5f63bebd592b5e0ecb
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-12 11:45:33 -0700

    set build folders from gulp build

---
+++ build/server.js
@@ -0,0 +1,4 @@
+"use strict";
+//# sourceMappingURL=server.js.map
+
+//# sourceMappingURL=server.js.map
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,7 @@
 import gulp from "gulp";
 import path from "path";
 
-const $ = require("gulp-load-plugin")();
+const $ = require("gulp-load-plugins")();
 
 gulp.task("server:build", () => {
   return gulp.src("./src/server/**/*.js")
@@ -9,6 +9,6 @@ gulp.task("server:build", () => {
     .pipe($.sourcemaps.init())
     .pipe($.babel())
     .pipe($.sourcemaps.write(".", {}))
-    .pipe($sourcemaps.write(".",{sourceRoot: path.join(__dirname, "src", "server")}))
+    .pipe($.sourcemaps.write(".",{sourceRoot: path.join(__dirname, "src", "server")}))
     .pipe(gulp.dest("./build"));
 });
