8f4fa447b273bb5496ae817edfa587f082f6f02d
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-13 13:07:07 -0700

    webpack set up

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,55 @@ const vendor = [
 function createConfig(isDebug) {
 	const devtool = isDebug ? cheap-module-source-map : null;
   const plugins = [
-      new webapack.optimize.CommonsChunkPlugin("")
-  ]
-  return {
+      new webapack.optimize.CommonsChunkPlugin("vendor", "vendor.js"),
+      new webapack.DefinrPlugin({
+        "process.env": {
+          NODE_ENV: `"${process.env.NODE_ENV || "development"}"`
+        },
+        IS_PRODUCTION: !isDebug,
+        IS_DEVELOPMENT: isDebug
+      })
+  ];
 
+  const loaders = {
+    js: { test: /\.jsx?$/, loader: "babel", exclude: /node_modules/},
+    eslint: { test: /\.jsx?$/, loader: "eslint", exclude: /node_modules/},
+    json: { test: /\.json$/,loader: "json" },
+    css: { test: /\.scss$/, loader: "style!css?sourceMap!sass?sourceMap"},
+    files: { test: /\.(png|jpg|jpeg|gif|woff|ttf|eot|svg|woff2)/, loader: "url-loader?limit=5000" }
+  }
+
+  const clientEntry = ["./src/client/client.js"];
+  let publicPath = "/build/";
+
+  if (isDebug) {
+
+  } else {
+
+  }
+
+  return {
+    name: client,
+    devtool,
+    entry: {
+      app: clientEntry,
+      vendor
+    },
+    output: {
+      path: path.join(__dirname, "public", "build"),
+      filename: "[name].js",
+      publicPath
+    },
+    resolve: {
+      extensions: ["", ".js", ".jsx"],
+      alias: {
+        shared: path.join(__dirname, "src", "server", "shared")
+      }
+    },
+    module: {
+      loaders: _.values(loaders)
+    }, 
+    plugins
   };
 }
 module.exports = createConfig(process.env.NODE_ENV !== "production");
