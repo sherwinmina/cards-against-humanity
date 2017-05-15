fb4da8a05ebc6ff605d1177006c6a5e6fe581c2c
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-15 13:53:29 -0700

    Clean up exports

--- src/client/client.js
+++ src/client/client.js
@@ -1,19 +1,16 @@
 import './client.scss';
 
-import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppContainer from './components/app';
-
 function main() {
-  const AppContainer = require("./components/app").default;
-  ReactDOM.render(<AppContainer />, document.getElementById('mount'));
+  const routes = require("./routes").default();
+  ReactDOM.render(routes, document.getElementById('mount'));
 }
 
 main();
 
 if (module.hot) {
-  module.hot.accept("./components/app", () => {
+  module.hot.accept("./routes", () => {
     main();
   });
 }
