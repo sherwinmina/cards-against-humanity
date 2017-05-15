c564484bd0dd5dfa5e20df08f7e65df330800f42
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-15 13:22:45 -0700

    hooked up initial react components

---
+++ src/client/components/app.js
@@ -0,0 +1,15 @@
+import './app.scss';
+
+import React, {Component} from 'react';
+
+class AppContainer extends Component {
+  render()  {
+    return (
+      <div>
+        <h1>Hello from React component</h1>
+      </div>
+    );
+  }
+}
+
+export default AppContainer;
