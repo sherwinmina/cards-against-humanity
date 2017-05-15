fed8150e88d30d53371063cb2ad8b56872df4b62
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-14 17:21:35 -0700

    set up font awesome to be on the public build

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ function createConfig(isDebug) {
 		files:	{ test: /\.(png|jpg|jpeg|gif|woff|ttf|eot|svg|woff2)/, loader: "url-loader?limit=5000" }
 	};
 
-	const clientEntry = ["babel-polyfill", "./src/client/client.js"];
+	const clientEntry = ["./src/client/client.js"];
 	let publicPath = "/build/";
 
 	if (isDebug) {
