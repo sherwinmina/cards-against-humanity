719809b08f242df57b1103e227c8ecae6878e201
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-15 22:05:54 -0700

    Working on displaying chat as list items

--- src/client/components/lobby.js
+++ src/client/components/lobby.js
@@ -9,19 +9,36 @@ import Chat from "./chat";
     this._joinGame = () => {
       console.log(`Join Game ${game.title}`);
     };
+
+     this._sendMessage = (message) => {
+      console.log(`Sending ${message}`);
+    };
   }
 
-   render () {
+  render () {
     const games = [
       {title: "Game 1", id: 1, players: ["one", "two", "three"]},
       {title: "Game 2", id: 2, players: ["John", "Joseph", "Mary"]},
       {title: "Game 3", id: 3, players: ["Jim", "Jake", "Akiko"]}
     ];
 
+    const opSendMessage = {can: true, inProgress: false};
+    const messages = [
+      {index: 1, name: "Alika", message: "Blegh"},
+      {index: 2, name: "Moana", message: "Hello"},
+      {index: 3, name: "Leilani", message: "Whatever"},
+      {index: 4, name: "Keiki", message: "Just got here"},
+      {index: 5, name: "Kealii", message: "aloha"},
+    ];
+
      return (
        <div className="c-lobby">
         <GameList games={games} joinGame={this._joinGame} />
-        <Chat />
+        <Chat 
+          messages={messages}
+          opSendMessage={opSendMessage}
+          _sendMessage={this._sendMessage}
+        />
        </div>
      );
    }
@@ -36,7 +53,8 @@ import Chat from "./chat";
     };
   }
 
-   render () {
+  
+  render () {
     const canLogin = true;
     const canCreategame = true;
     const createGameInProgress = false;
