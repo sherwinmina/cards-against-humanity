5cbc41d4c5a24bd0784158d7350ea42b41525288
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-16 11:26:26 -0700

    working on dispatcher

--- src/server/shared/dispatcher.js
+++ src/server/shared/dispatcher.js
@@ -3,5 +3,38 @@ import {observable} from 'rxjs';
 import * as A from './actions';
 
 export class Dispatcher {
-  
-}
+	constructor() {
+		this._handlers = {};
+		this._emitBuffer = [];
+		this._inEmit = false;
+	}
+
+on(typeOrCallbacks, callback = null, statusFilter = null) {
+		if (_.isObject(typeOrCallbacks)) {
+			const unreg = _.map(
+				typeOrCallbacks,
+				(callback, type) => this.on(type, callback, statusFilter));
+
+			return () => unreg.forEach(unsub => unsub());
+		}
+
+		if (!_.isFunction(callback))
+			throw new Error("callback must be of type function");
+
+		const type = typeOrCallbacks;
+		const handler = {statusFilter, callback};
+
+		if (!this._handlers.hasOwnProperty(type))
+			this._handlers[type] = [handler];
+		else
+			this._handlers[type].push(handler);
+
+		return () => {
+			const handlers = this._handlers[type];
+			const index = handlers.indexOf(handler);
+			if (index == -1)
+				return;
+
+			handlers.splice(index, 1);
+		};
+	}
