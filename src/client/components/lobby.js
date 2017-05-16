ec9583ccc79aeb61e55a34fe920f91bc0334036e
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-15 18:09:34 -0700

    added conditional rendering for lobby

--- src/client/components/lobby.js
+++ src/client/components/lobby.js
@@ -3,23 +3,73 @@ import './lobby.scss';
  import React, { Component } from 'react';
  
  class LobbyContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this._joinGame = () => {
+      console.log(`Join Game ${game.title}`);
+    };
+  }
+
    render () {
+    const games = [];
+
      return (
-       <div>
-         <p>LOBBY!</p>
+       <div className="c-lobby">
+        <GameList games={games} joinGame={this._joinGame} />
        </div>
      );
    }
  }
 
  class LobbySidebar extends Component {
+  constructor(props) {
+    super(props);
+
+    this._login = () => {
+      console.log("IMPLEMENT LOGIN");
+    };
+  }
+
    render () {
+    const canLogin = true;
+    const canCreategame = true;
+    const createGameInProgress = false;
+
      return (
-       <div>
-         <p>lobbySidebar</p>
-       </div>
+       <section className="c-lobby-sidebar">
+         <div className="m-sidebar-buttons">
+          {!canLogin ? null : 
+            <button className="m-button primary" onClick={this._login}>Login</button>}
+          {!canCreategame ? null :
+            <button 
+              onClick={this._createGame}
+              disabled={createGameInProgress}
+              className="m-button good">
+              Create Game
+            </button>}
+         </div> 
+       </section>
      );
    }
+ }
+
+ function GameList({games, joinGame}) {
+   return (
+    <section className="c-game-list">
+      {games.length > 0 ? null : 
+        <div className="no-games">There are no games yet</div>}
+
+      {games.map(game => 
+        <div className="game" key={game.id} onClick={() => joinGame(game)}>
+          <div className="title">{game.title}</div>
+          <div className="players">
+           {game.players.join(", ")}
+          </div>
+          <div className="join-game">Join Game</div>
+        </div>)}
+    </section>
+   );
  }
  
  export default {
