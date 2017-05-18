4367853ddb0b06ce4ac75906df5c424d3c988267
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-17 17:06:43 -0700

    Set up index Store

--- src/client/stores/index.js
+++ src/client/stores/index.js
@@ -4,5 +4,12 @@ import LobbyStore from './lobby';
 import UserStore from './store';
 
 export default function(services) {
-  
+  const user = new UserStore(services);
+  const game = new GamesStore(services, user);
+  const lobby = new LobbyStore(services, user);
+  const app = new AppStore(services);
+
+  return {
+    user, game, lobby, app
+  };
 }
