fed24ab595c0957c4b2d298c3e0568bff5bc8517
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-18 11:57:24 -0700

    Hooked up store provider

--- src/client/lib/component.js
+++ src/client/lib/component.js
@@ -7,6 +7,16 @@ export class StoreProvider extends Component {
     services: PropTypes.object.isRequired
   };
 
+  static childContextTypes = {
+    stores: PropTypes.object.isRequired,
+    services: PropTypes.object.isRequired
+  };
+
+  getChildContext() {
+    const {stores, services } = this.props;
+    return {stores, services};
+  }
+
   render() {
     return Children.only(this.props.children);
   }
