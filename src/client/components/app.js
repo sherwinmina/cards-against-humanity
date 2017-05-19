9902de3905e615db90a04108f9ce9ce4dafdd47c
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-19 09:25:38 -0700

    Instantiate Dialog

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -6,13 +6,19 @@ import {ContainerBase} from "../lib/component";
 import dialogTypes from "./dialogs";
 
 class AppContainer extends ContainerBase {
-  componentDidMount() {
+  componentWillMount() {
     const { stores: {app}} = this.context;
     this.subscribe(app.dialogs$, dialogs => this.setState({dialogs}));
   }
 
   render()  {
     const {main, sidebar} = this.props;
+    const {dialogs} = this.state;
+
+    const dialogStack = dialogs.map(dialog => {
+      const DialogComponent = dialogTypes[dialog.id];
+      return <DialogComponent {...dialog.props} key={dialog.id} />;
+    });
 
     return (
       <div className={`c-application`}>
