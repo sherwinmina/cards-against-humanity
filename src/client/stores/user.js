2260a59ddbefe33963799cb800e565ca8f9a8aff
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-23 17:22:10 -0700

    added observables on user

--- src/client/stores/user.js
+++ src/client/stores/user.js
@@ -1,5 +1,18 @@
+import{Observable, BehaviorSubject} from 'rxjs';
 import * as A from '../actions';
 
+const defaultDeatails = {
+  isLiggedIn: false,
+  id: null,
+  name: null
+};
+
 export default class UserStore {
+  constructor({dispatcher}) {
+    this.details$ = new BehaviorSubject(defaultDeatails);
+
+    this.details$.subscribe(details => 
+      Object.keys(details).forEach(k => this[k] = details[k]));
+  }
 
 }
