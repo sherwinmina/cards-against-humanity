049d29c6f5bc38cdc097de233ec12cb8aecbf567
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-28 00:29:59 -0700

    render sidebar

--- src/client/components/game.js
+++ src/client/components/game.js
@@ -42,11 +42,24 @@ class GameSidebar extends ContainerBase {
     const {opLogin, game} = this.state;
 
     return (
-      <div>
-          <p>GAME SIDEBAR!</p>
-      </div>
-    );
+      <section className="c-game-sidebar">
+				<div className="m-sidebar-buttons">
+					{!opLogin.can ? null :
+						<button className="m-button primary" onClick={this._login}>
+							Login to join game
+						</button>}
+
+					<button className="m-button" onClick={this._exitGame}>Leave game</button>
+				</div>
+				{game.step == A.STEP_DISPOSED ? null :
+					<PlayerList players={game.players} />}
+			</section>
+		);
   }
+}
+
+function PlayerList({players}) {
+  return <p> players.map( p => p.name).join(", ")</p>;
 }
 
 export default {
