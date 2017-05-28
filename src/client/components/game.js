381381278954ade6ca37d84660560baedbf6eea4
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-28 00:06:45 -0700

    hooked up to the game store

--- src/client/components/game.js
+++ src/client/components/game.js
@@ -25,7 +25,22 @@ class GameContainer extends ContainerBase {
 }
 
 class GameSidebar extends ContainerBase {
+  constructor(props) {
+		super(props);
+
+		this._exitGame = () => this.props.router.push("/");
+		this._login = () => this.dispatch(A.dialogSet(A.DIALOG_LOGIN, true));
+	}
+
+	componentWillMount() {
+		const {stores: {user, game}} = this.context;
+		this.subscribe(user.opLogin$, opLogin => this.setState({opLogin}));
+		this.subscribe(game.view$, game => this.setState({game}));
+	}
+
   render () {
+    const {opLogin, game} = this.state;
+
     return (
       <div>
           <p>GAME SIDEBAR!</p>
