784ad84705c384b291b966cc3441b6f7f8735447
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-29 12:22:29 -0700

    Empty component set up

---
+++ src/client/components/game/player-hand.js
@@ -0,0 +1,9 @@
+import "./player-hand.scss";
+
+import react from "react";
+import Card from "./card";
+
+export default function Playerhand(props) {
+  const {hand, opSelectCard, selectCard, toggle, isOpen} = props;
+  return <p>Playerhand</p>;
+}
---
+++ src/client/components/game/stack.js
@@ -0,0 +1,10 @@
+import "./stacks.scss";
+
+import React from "react";
+import Card from "./card";
+
+export default function Stacks(props) {
+  const {stacks, opSelectedStack, selectStack, ourStackId} = props;
+
+  return <p>Stacks</p>;
+}
