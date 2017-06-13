523334bbe46b06e432ee98e145342d7ab0ce2acd
Sherwin Mina <sherwinmina@yahoo.com>
2017-06-13 14:49:03 -0700

    draw white cards

--- src/server/models/card.js
+++ src/server/models/card.js
@@ -58,3 +58,25 @@ export class CardDatabase {
       return new Deck(whiteCards, blackCards);
   }
 }
+
+export class Deck {
+  constructor(whiteCards, blackCards) {
+    this._whiteDeck = whiteCards;
+    this._blackDeck = blackCards;
+    this._whiteDiscard = [];
+    this.blackIndex = 0;
+  }
+
+  drawWhiteCards(count) {
+    if (count >= this._whiteDeck.length) {
+      if (count >= this._whiteDeck.length + this._whiteDiscard.length) 
+        throw new Error(`Cannot draw ${count} cards, since there aren't enough left`);
+
+        this._whiteDeck.push(...this._whiteDiscard);
+        this._whiteDiscard = [];
+        shuffle(this._whiteDeck);
+    }    
+
+    return this._whiteDeck.splice(0, count);
+  }
+}
