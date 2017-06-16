ee173a3a88b0b3d4dbb6ae7ca29f4f93526abd84
Sherwin Mina <sherwinmina@yahoo.com>
2017-06-16 13:05:19 -0700

    broadcast room action

--- src/server/lib/room.js
+++ src/server/lib/room.js
@@ -17,5 +17,39 @@ export class RoomBase {
 
   addClient(client) {
     this.clients.push(client);
+    client.emit(A.setView(this._viewId, this.view, this.id));
+    return () => _.remove(this.clients, {id: client.id});
+  }
+
+  broadcast(action) {
+    for (let client of this.clients)
+      client.emit(action);
+  }
+
+  _tick(action) {
+    if(this._inTick) {
+      if (action) 
+        action();
+        return null;
+    }
+
+    this._inTick = true;
+    if (action) {
+      try {
+        action();
+      } finally {
+        this._inTick = false;
+      }
+    }
+
+    this._postTick();
+    const newView = this.view;
+    const diff = makeDiff(this._lastView, newView);
+
+    if (diff != IS_UNCHANGED)
+      this.broadcast(A.mergeView(this._viewId, diff, this.id));
+      
+    this._lastView = newView;
+    return diff;
   }
 }
