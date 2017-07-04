c8e9b57570847017cbb53a65daf19e1cb8dd8dfe
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-04 10:03:56 -0700

    added lobby model

---
+++ src/server/model/handlers/lobby.js
@@ -0,0 +1,13 @@
+import * as A from "../../actions";
+import {HandlerBase} from "../../lib/handler";
+import {validateMessage} from " ../../shared/validation";
+
+export default class LobbyHandlers extends HandlerBase {
+  constructor(client, lobby) {
+    super(client, lobby) {
+      super(client);
+
+      this.inDispose();
+    }
+  }
+}
