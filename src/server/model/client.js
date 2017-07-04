a25d84f10322d55af5007f04a883020a585528da
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-03 20:56:59 -0700

    handling user validation on the server

--- src/server/model/client.js
+++ src/server/model/client.js
@@ -1,6 +1,8 @@
 import * as A from '../actions';
 import {Dispatcher} from '../shared/dispatcher';
 
+import {validateName} from '../shared/validation/user';
+
 export class Client extends Dispatcher {
   get details() {
     return {
@@ -33,12 +35,24 @@ export class Client extends Dispatcher {
     this._socket.emit("action", action);
   }
 
+  login(name) {
+    const validator = validateName(name);
+    if (validator.didFail) 
+      return validator;
+
+    this.LoggedIn = true;
+    this.name = name;
+    this.emit(A.userDetailsSet(this.details));
+  }
+
   dispose() {
     this.onDisposes.forEach(a => a());
     this._onDisposes = [];
   }
 
   _installHandlers() {
-
+    this.onRequest({
+      [A.USER_LOGIN]: 
+    })
   }
 }
