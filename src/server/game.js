6e3b24c97d43c3d3e60fea8c7bec704d478b057c
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-07 08:41:34 -0700

    finished game view

--- src/server/game.js
+++ src/server/game.js
@@ -6,3 +6,17 @@ import {Player} from "./player";
 
 export const MINIMUM_PLAYERS = 2;
 
+export class Game extends RoomBase {
+  get view() {
+    return {
+      id: this.id,
+      title: this.title,
+      step: this.step,
+      options: this.options,
+      players: this.players.map(player => player.summary),
+      messages: this.messages.slice(),
+      round: this.round && this.round.view,
+      timer: this.timer
+    };
+  }
+}
