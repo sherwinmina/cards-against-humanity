41df2dc11aa6de06f81b58f511488540f30eeca3
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-07 08:36:44 -0700

    added game handler

---
+++ src/server/game.js
@@ -0,0 +1,8 @@
+import _ from "lodash";
+import * as A from "../actions";
+import {Validator} from "../shared/validation";
+import {RoomBase} from "../lib/room";
+import {Player} from "./player";
+
+export const MINIMUM_PLAYERS = 2;
+
